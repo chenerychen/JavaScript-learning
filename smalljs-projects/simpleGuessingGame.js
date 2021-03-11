a6f92812ff63e69b0559708500c3d3ac2a934b94
chenerychen <zhaocsnh@gmail.com>
2021-03-11 11:56:47 -0800

    first commit with a simple number guessing game

---
+++ smalljs-projects/simpleGuessingGame.js
@@ -0,0 +1,27 @@
+
+
+
+let maxNum = parseInt(prompt("Enter a max number"));
+
+while (!maxNum) {
+  maxNum = parseInt(prompt("Enter a valid number"))
+}
+
+let randomNum = Math.floor(Math.random() * maxNum) + 1; 
+
+let guess = parseInt(prompt("Enter your guess number"))
+
+
+let attempts = 1; 
+
+while (guess !== maxNum) {
+  if (guess > maxNum) {
+    guess = parseInt(prompt("Too big"))
+  }
+  else {
+    guess = parseInt(prompt("Too small"))
+  }
+  attempts++
+}
+
+console.log(`You got it at ${attempts}`)

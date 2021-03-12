97c3a55f790d0ff1e86a406fefde5ef5bcbf33e4
chenerychen <zhaocsnh@gmail.com>
2021-03-11 16:11:45 -0800

    Added some comments on the top to descirbe this program

--- small-js-projects/simpleGuessingGame.js
+++ small-js-projects/simpleGuessingGame.js
@@ -1,3 +1,13 @@
+/* 
+1. Generate a random number from user and set it to the max number
+2. Ensure that the user input will be a valid number 
+3. Add attempts to keep track of the time of user input before the correct guess
+4. Add q for quit the program
+5. Add a boolean to display messages to guide users
+6. Display message for quitting the program or get the correct answer
+
+*/ 
+
 let maxNum = parseInt(prompt("Enter a max number"));
 
 while (!maxNum) {

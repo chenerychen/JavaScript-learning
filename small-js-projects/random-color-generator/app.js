c114eaece9c0da10e3687d890f66343cba003996
chenerychen <zhaocsnh@gmail.com>
2021-03-16 10:18:05 -0400

    moved generating random numbers into a function that can be reuseld elsewhere

--- small-js-projects/random-color-generator/app.js
+++ small-js-projects/random-color-generator/app.js
@@ -4,11 +4,12 @@
 const btn = document.querySelector("#btn")
 
 btn.addEventListener('click', () => {
-    let r = Math.floor(Math.random() * 256)  + 1; 
-    let g = Math.floor(Math.random() * 256)  + 1; 
-    let b = Math.floor(Math.random() * 256)  + 1; 
+    // let r = Math.floor(Math.random() * 256)  + 1; 
+    // let g = Math.floor(Math.random() * 256)  + 1; 
+    // let b = Math.floor(Math.random() * 256)  + 1; 
+    // let newColor = `rgb(${r}, ${g}, ${b})`
 
-    let newColor = `rgb(${r}, ${g}, ${b})`
+    let newColor = generateRandNumbers()
     document.body.style.backgroundColor = newColor;
 
     let colorDisplay = document.querySelector("#colorDisplay")
@@ -17,3 +18,11 @@ btn.addEventListener('click', () => {
     let btn = document.querySelector('#btn')
     btn.style.backgroundColor = `rgb(100,101,103)`
 })
+
+const generateRandNumbers = () => {
+    let r = Math.floor(Math.random() * 256)  + 1; 
+    let g = Math.floor(Math.random() * 256)  + 1; 
+    let b = Math.floor(Math.random() * 256)  + 1; 
+
+    return  `rgb(${r}, ${g}, ${b})`
+}

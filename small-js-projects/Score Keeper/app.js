e6055942b9d3f0e0035dba541dae9ca782c6f102
chenerychen <zhaocsnh@gmail.com>
2021-03-17 14:49:58 -0400

    added Score Keeper to the repo. Score Keeper is a part of Colt Steele's online project. There were a few challenging concepts and the CSS part is done by Bulma. The challenging part was that the variable scores need to be outside of function and to disable the user from increament number which the side is not inline with the highest score and get the winning score from the select

---
+++ small-js-projects/Score Keeper/app.js
@@ -0,0 +1,67 @@
+
+
+const player1Plus = document.querySelector("#player1Plus"); 
+const player2Plus = document.querySelector("#player2Plus"); 
+const reset = document.querySelector('#reset')
+const h1 = document.querySelector('h1')
+let p1Scores  = document.querySelector("#p1Scores"); 
+let p2Scores = document.querySelector("#p2Scores"); 
+
+let  topScores = document.querySelector("#topScores")
+let winningScore = 3; 
+let p1Score = 0; 
+let p2Score = 0; 
+
+let isGameOver = false; 
+
+topScores.addEventListener("change", () => {
+    winningScore = parseInt(topScores.value) 
+    resetFunc();                                                        // inside of a callback Function, needs to be evaluated to a value. 
+})
+
+player1Plus.addEventListener('click',  () => {
+    if(!isGameOver) {
+        p1Score+=1; 
+        if (p1Score === winningScore) {
+            isGameOver = true;
+            p1Scores.classList.add('has-text-success')
+            p2Scores.classList.add('has-text-danger')
+            player1Plus.disabled = true; 
+            player2Plus.disabled = true; 
+        } 
+         p1Scores.innerText = p1Score;
+
+    }
+})
+
+player2Plus.addEventListener('click',  () => {
+    if(!isGameOver) {
+        p2Score+=1; 
+        if (p2Score === winningScore) {
+            isGameOver = true;
+            p2Scores.classList.add('has-text-success')
+            p1Scores.classList.add('has-text-danger')
+            player1Plus.disabled = true; 
+            player2Plus.disabled = true; 
+        } 
+         p2Scores.innerText = p2Score;
+    }
+})
+
+reset.addEventListener('click', resetFunc)  
+
+
+function resetFunc()  {
+    isGameOver = false; 
+    p1Score = 0; 
+    p2Score = 0; 
+    p1Scores.innerText = 0; 
+    p2Scores.innerText = 0; 
+    p1Scores.classList.remove('has-text-success', 'has-text-danger')
+    p2Scores.classList.remove('has-text-success', 'has-text-danger')
+    player1Plus.disabled = false; 
+    player2Plus.disabled = false;  
+}
+
+
+
